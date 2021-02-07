67bee039064129fe77984ef9d40152be7ae53f97
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-02-07 18:51:12 +0100

    Update app_st.py

--- app_st.py
+++ app_st.py
@@ -90,10 +90,10 @@ for num_row in range(2):
         for i in range(len(importance)):
             if importance[i] < 0:
                 importance[i] *= -1
-        st.error(importance)
-        dict_fin = {list(train_nm)[i]: importance[i] for i in range(importance.shape[0])}
+        st.error(fea_Imp_features)
+        dict_fin = {train_nm.columns[i]: importance[i] for i in range(importance.shape[0])}
         dict_fin = {k: v for k, v in sorted(dict_fin.items(), key=lambda item: item[1], reverse = True)}
-        dict_fin_per = {list(train_nm)[i]: (importance[i] / np.sum(importance)) * 100 for i in range(importance.shape[0])}
+        dict_fin_per = {train_nm.columns[i]: (importance[i] / np.sum(importance)) * 100 for i in range(importance.shape[0])}
         dict_fin_per = {k: v for k, v in sorted(dict_fin_per.items(), key=lambda item: item[1], reverse = True)}
         lis_final = []; res_par = 0
         for value in dict_fin_per.values():
